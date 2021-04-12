cf8e40bd3e12f3593e516966929794d5057cb3e9
mooniljoo <ezekielmoon@naver.com>
2021-04-12 18:25:48 +0900

    Update Script 3rd ver.

--- parsing-seoul_auction.js
+++ parsing-seoul_auction.js
@@ -1,32 +1,38 @@
 let q = [];
 let ls = document.querySelectorAll("#auctionList > li")
+let source = document.querySelector("title").innerText
 ls.forEach((item) => {
-const number = item.querySelector('.auction_lotnum').innerText;
-const artist = item.querySelector('[ng-bind="lot.ARTIST_NAME_JSON[locale]').innerText;
-const birth = item.querySelector('.auction_workartist > .txt_pale ').innerText;
-const title = item.querySelector('.auction_worktitle').innerText;
+const number = item?.querySelector('.auction_lotnum > strong')?.innerText;
+const artistInfo = item?.querySelector('.auction_workartist');
+const artist = artistInfo?.querySelectorAll('.workartist > span')[0].innerText;
+const birth = artistInfo?.querySelectorAll('.workartist > span')[1].innerText;
+const title = item?.querySelector('.auction_worktitle')?.innerText;
+
+const artistKr = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(artist) ? artist : '';
+const artistEn = !/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(artist) ? artist : '';
 
 const titleKr = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(title) ? title : '';
 const titleEn = !/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(title) ? title : '';
 
-const desc = item.querySelector('.workmaterial')
-const material = desc.querySelector('[ng-bind="lot.MATE_NM_EN"]')?.innerText;
+const desc = item?.querySelector('.workmaterial')
+const material = desc?.querySelector('[ng-bind="lot.MATE_NM_EN"]')?.innerText;
 
-const sizeEdition = item.querySelector('.edition_txt_p')?.innerText || '';
+const sizeEdition = item?.querySelector('.edition_txt_p')?.innerText || '';
 
 const year = desc?.querySelector('.ng-binding')?.innerText || '';
 const estimate = item?.querySelector('li.es_price_right')?.innerText.replace('\n', ' ') || '';
 
 q.push({
 number,
-artist,
-birth,
+artistKr,
+artistEn,
 titleKr,
 titleEn,
 year,
-material,
 sizeEdition,
-estimate
+material,
+estimate,
+source
 })
 })
 copy(q)
