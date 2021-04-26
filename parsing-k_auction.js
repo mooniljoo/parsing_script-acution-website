975c7b3d00c8412fcdd5f09de15cf360e07860dd
mooniljoo <ezekielmoon@naver.com>
2021-04-26 13:41:53 +0900

    Fix : Handling when sizeEditoniYear is empty

--- parsing-k_auction.js
+++ parsing-k_auction.js
@@ -4,19 +4,15 @@ let source = document.querySelector("title")?.innerText;
 let auction_title = document.querySelector('.subtop-desc > h1')?.innerText;
 ls.forEach((item, index) => {
   const mainInfo = item.querySelector(".card.artwork");
-  const number =
-    mainInfo?.querySelector(".lot")?.innerText.split("LOT ")[1] || "";
+  const number = mainInfo?.querySelector(".lot")?.innerText.split("LOT ")[1] || "";
   const artist = mainInfo?.querySelector(".card-title")?.innerText || "";
   const title = mainInfo?.querySelector(".card-subtitle")?.innerText || "";
   const description = mainInfo?.querySelector(".description")?.innerText || "";
-  const material = description.split("\n")[0];
-  const sizeEditionYear = description.split("\n")[1];
-  const sizeEdition = sizeEditionYear.split(" |")[0];
-  const year = sizeEditionYear.split("| ")[1];
-  const estimate =
-    mainInfo
-      ?.querySelector(".list-inline-item.font-numbers")
-      ?.innerText.replace("\n", " ") || "";
+  const material = description?.split("\n")[0] || "";
+  const sizeEditionYear = description?.split("\n")[1] || "";
+  const sizeEdition = sizeEditionYear?.split(" |")[0] || "";
+  const year = sizeEditionYear?.split("| ")[1] || "";
+  const estimate = mainInfo?.querySelector(".list-inline-item.font-numbers")?.innerText.replace("\n", " ") || "";
 
   const artistKr = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(artist) ? artist : "";
   const artistEn = !/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(artist) ? artist : "";
